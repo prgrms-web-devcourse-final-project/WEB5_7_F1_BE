3bb764ca5f9723b7630e974ab6cf92c512046c6d
limkanghyun <limkh06910691@gmail.com>
2025-07-11 15:00:49 +0900

    :sparkles: feat: SSE 기본 기능 구현

---
+++ backend/src/main/java/io/f1/backend/domain/game/sse/SseService.java
@@ -0,0 +1,29 @@
+package io.f1.backend.domain.game.sse;
+
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@Service
+@RequiredArgsConstructor
+public class SseService {
+
+    private final SseEmitterRepository emitterRepository;
+
+    public SseEmitter subscribe() {
+        SseEmitter emitter = new SseEmitter(30_000L);
+        emitterRepository.save(emitter);
+
+        try {
+            // emitter 정상 전송확인 메시지
+            emitter.send(SseEmitter.event()
+                .name("connect")
+                .data("connected"));
+        } catch (IOException e) {
+            // emitter send() 호출 시 예외 처리
+            emitterRepository.remove(emitter);
+        }
+        return emitter;
+    }
+}
