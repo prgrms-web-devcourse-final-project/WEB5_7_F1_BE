da50e5db14581273841c5533b7c7178b7298e0ea
kanghyun <limkh06910691@gmail.com>
2025-07-13 19:01:20 +0900

    ✨ feat: 게임 방 전체 목록 조회 구현
    * :wrench: chore: Quiz, User에 @Getter 및 임시 @Setter 추가
    
    * chore: Java 스타일 수정
    
    * :sparkles: feat: 룸 목록 조회 응답 DTO(RoomListResponse) 추가
    - RoomResponse 추가
    
    * chore: Java 스타일 수정
    
    * :sparkles: feat: 룸 전체 조회 API 구현
    
    * chore: Java 스타일 수정
    
    * :white_check_mark: test: 룸 전체 조회 리포지토리 테스트 추가
    
    * chore: Java 스타일 수정
    
    * :recycle: 리뷰 반영 수정
    - RoomService의 RoomMapper를 static import로 변경
    - roomResponse DTO의 title을 roomName으로 변경
    
    ---------
    
    Co-authored-by: github-actions <>

--- backend/src/main/java/io/f1/backend/domain/game/api/RoomController.java
+++ backend/src/main/java/io/f1/backend/domain/game/api/RoomController.java
@@ -3,12 +3,14 @@ package io.f1.backend.domain.game.api;
 import io.f1.backend.domain.game.app.RoomService;
 import io.f1.backend.domain.game.dto.request.RoomCreateRequest;
 import io.f1.backend.domain.game.dto.response.RoomCreateResponse;
+import io.f1.backend.domain.game.dto.response.RoomListResponse;
 
 import jakarta.validation.Valid;
 
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,5 +36,10 @@ public class RoomController {
         loginUser.put("nickname", "빵야빵야");
 
         return roomService.saveRoom(request, loginUser);
+    }
+
+    @GetMapping
+    public RoomListResponse getAllRooms() {
+        return roomService.getAllRooms();
     }
 }
--- backend/src/main/java/io/f1/backend/domain/game/mapper/RoomMapper.java
+++ backend/src/main/java/io/f1/backend/domain/game/mapper/RoomMapper.java
@@ -1,12 +1,30 @@
 package io.f1.backend.domain.game.mapper;
 
 import io.f1.backend.domain.game.dto.request.RoomCreateRequest;
+import io.f1.backend.domain.game.dto.response.RoomResponse;
+import io.f1.backend.domain.game.model.Room;
 import io.f1.backend.domain.game.model.RoomSetting;
+import io.f1.backend.domain.quiz.entity.Quiz;
 
 public class RoomMapper {
 
     public static RoomSetting toRoomSetting(RoomCreateRequest request) {
         return new RoomSetting(
                 request.roomName(), request.maxUserCount(), request.locked(), request.password());
+    }
+
+    public static RoomResponse toRoomResponse(Room room, Quiz quiz) {
+        return new RoomResponse(
+                room.getId(),
+                room.getRoomSetting().roomName(),
+                room.getRoomSetting().maxUserCount(),
+                room.getPlayerSessionMap().size(),
+                room.getRoomSetting().locked(),
+                room.getState().name(),
+                quiz.getTitle(),
+                quiz.getDescription(),
+                quiz.getCreator().getNickname(),
+                quiz.getQuestions().size(),
+                quiz.getThumbnailUrl());
     }
 }
