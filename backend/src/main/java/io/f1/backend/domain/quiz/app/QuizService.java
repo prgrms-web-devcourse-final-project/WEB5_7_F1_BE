a0fd2f994013051b2b5efc9de07d2bb2ed494d5a
silver-eunjoo <leeeun5423@naver.com>
2025-07-14 17:36:47 +0900

    :recycle: refactor : StringUtils.isBlank 적용 및 로그 수정

--- backend/src/main/java/io/f1/backend/domain/quiz/app/QuizService.java
+++ backend/src/main/java/io/f1/backend/domain/quiz/app/QuizService.java
@@ -19,8 +19,11 @@ import io.f1.backend.domain.quiz.entity.Quiz;
 import io.f1.backend.domain.user.dao.UserRepository;
 import io.f1.backend.domain.user.entity.User;
 
+import io.micrometer.common.util.StringUtils;
+import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -35,6 +38,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class QuizService {
@@ -110,7 +114,8 @@ public class QuizService {
                         .findById(quizId)
                         .orElseThrow(() -> new NoSuchElementException("존재하지 않는 퀴즈입니다."));
 
-        if (1L != quiz.getCreator().getId()) {
+        // TODO : util 메서드에서 사용자 ID 꺼내쓰는 식으로 수정하기
+        if(1L != quiz.getCreator().getId()) {
             throw new RuntimeException("권한이 없습니다.");
         }
 
@@ -156,13 +161,13 @@ public class QuizService {
 
         try {
             boolean deleted = deleteIfExists(filePath);
-            if (deleted) {
-                System.out.println("기존 썸네일 삭제 완료 : " + filePath);
+            if( deleted ) {
+                log.info("기존 썸네일 삭제 완료 : {}", filePath);
             } else {
-                System.out.println("기존 썸네일 존재 X : " + filePath);
+                log.info("기존 썸네일 존재 X : {}", filePath);
             }
         } catch (IOException e) {
-            System.err.println("기존 썸네일 삭제 중 오류 : " + filePath);
+            log.error("기존 썸네일 삭제 중 오류 : {}", filePath);
             throw new RuntimeException(e);
         }
     }
@@ -173,9 +178,9 @@ public class QuizService {
         Page<Quiz> quizzes;
 
         // 검색어가 있을 때
-        if (title != null && !title.isBlank()) {
+        if(StringUtils.isBlank(title)) {
             quizzes = quizRepository.findQuizzesByTitleContaining(title, pageable);
-        } else if (creator != null && !creator.isBlank()) {
+        } else if(StringUtils.isBlank(creator)) {
             quizzes = quizRepository.findQuizzesByCreator_NicknameContaining(creator, pageable);
         } else { // 검색어가 없을 때 혹은 빈 문자열일 때
             quizzes = quizRepository.findAll(pageable);
