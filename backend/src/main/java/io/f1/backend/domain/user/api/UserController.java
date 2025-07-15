4f55482568afc9fec059302c712e1dafaf8b2258
jiwon1217 <koagjw@gmail.com>
2025-07-15 11:52:14 +0900

    :sparkles: feat: 닉네임 수정 기능 구현

--- backend/src/main/java/io/f1/backend/domain/user/api/UserController.java
+++ backend/src/main/java/io/f1/backend/domain/user/api/UserController.java
@@ -3,6 +3,7 @@ package io.f1.backend.domain.user.api;
 import static io.f1.backend.global.util.SecurityUtils.logout;
 
 import io.f1.backend.domain.user.app.UserService;
+import io.f1.backend.domain.user.dto.SignupRequestDto;
 import io.f1.backend.domain.user.dto.UserPrincipal;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -24,6 +27,15 @@ public class UserController {
         @AuthenticationPrincipal UserPrincipal userPrincipal, HttpSession httpSession) {
         userService.deleteUser(userPrincipal.getUserId());
         logout(httpSession);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> updateNickname(
+        @AuthenticationPrincipal UserPrincipal userPrincipal,
+        @RequestBody SignupRequestDto signupRequest, HttpSession httpSession) {
+        userService.updateNickname(userPrincipal.getUserId(), signupRequest.nickname(),
+            httpSession);
         return ResponseEntity.noContent().build();
     }
 }
