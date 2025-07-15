55019972c5c363514d3cba3ce77df2852da9351e
Lee Eunjoo (Silver) <80297399+silver-eunjoo@users.noreply.github.com>
2025-07-15 09:19:12 +0900

    :sparkles: 퀴즈 조회, 수정, 삭제 API 구현 (#31)
    * :sparkles: 퀴즈 조회, 수정, 삭제 API 구현
    
    * chore: Java 스타일 수정
    
    * :recycle: refactor : StringUtils.isBlank 적용 및 로그 수정
    
    * chore: Java 스타일 수정
    
    * :sparkles: feat: 퀴즈 목록 조회 최신순으로 정렬
    
    * chore: Java 스타일 수정
    
    * chore: Java 스타일 수정
    
    * :recycle: refactor : default 문자열 상수로 빼기
    
    * :recycle: refactor : StringUtils import문 수정, 메서드명 수정
    
    * chore: Java 스타일 수정
    
    ---------
    
    Co-authored-by: github-actions <>

--- backend/src/main/java/io/f1/backend/domain/quiz/app/QuizService.java
+++ backend/src/main/java/io/f1/backend/domain/quiz/app/QuizService.java
@@ -1,20 +1,31 @@
 package io.f1.backend.domain.quiz.app;
 
+import static io.f1.backend.domain.quiz.mapper.QuizMapper.pageQuizToPageQuizListResponse;
 import static io.f1.backend.domain.quiz.mapper.QuizMapper.quizCreateRequestToQuiz;
 import static io.f1.backend.domain.quiz.mapper.QuizMapper.quizToQuizCreateResponse;
+import static io.f1.backend.domain.quiz.mapper.QuizMapper.toQuizListPageResponse;
+
+import static java.nio.file.Files.deleteIfExists;
 
 import io.f1.backend.domain.question.app.QuestionService;
 import io.f1.backend.domain.question.dto.QuestionRequest;
 import io.f1.backend.domain.quiz.dao.QuizRepository;
 import io.f1.backend.domain.quiz.dto.QuizCreateRequest;
 import io.f1.backend.domain.quiz.dto.QuizCreateResponse;
+import io.f1.backend.domain.quiz.dto.QuizListPageResponse;
+import io.f1.backend.domain.quiz.dto.QuizListResponse;
+import io.f1.backend.domain.quiz.dto.QuizUpdateRequest;
 import io.f1.backend.domain.quiz.entity.Quiz;
 import io.f1.backend.domain.user.dao.UserRepository;
 import io.f1.backend.domain.user.entity.User;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -23,8 +34,10 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.UUID;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class QuizService {
@@ -34,6 +47,8 @@ public class QuizService {
 
     @Value("${file.default-thumbnail-url}")
     private String defaultThumbnailPath;
+
+    private final String DEFAULT = "default";
 
     // TODO : 시큐리티 구현 이후 삭제해도 되는 의존성 주입
     private final UserRepository userRepository;
@@ -92,6 +107,92 @@ public class QuizService {
         return filename.substring(filename.lastIndexOf(".") + 1);
     }
 
+    @Transactional
+    public void deleteQuiz(Long quizId) {
+
+        Quiz quiz =
+                quizRepository
+                        .findById(quizId)
+                        .orElseThrow(() -> new NoSuchElementException("존재하지 않는 퀴즈입니다."));
+
+        // TODO : util 메서드에서 사용자 ID 꺼내쓰는 식으로 수정하기
+        if (1L != quiz.getCreator().getId()) {
+            throw new RuntimeException("권한이 없습니다.");
+        }
+
+        deleteThumbnailFile(quiz.getThumbnailUrl());
+        quizRepository.deleteById(quizId);
+    }
+
+    @Transactional
+    public void updateQuiz(Long quizId, MultipartFile thumbnailFile, QuizUpdateRequest request)
+            throws IOException {
+
+        Quiz quiz =
+                quizRepository
+                        .findById(quizId)
+                        .orElseThrow(() -> new NoSuchElementException("존재하지 않는 퀴즈입니다."));
+
+        if (request.title() != null) {
+            quiz.changeTitle(request.title());
+        }
+
+        if (request.description() != null) {
+            quiz.changeDescription(request.description());
+        }
+
+        if (thumbnailFile != null && !thumbnailFile.isEmpty()) {
+            validateImageFile(thumbnailFile);
+            String newThumbnailPath = convertToThumbnailPath(thumbnailFile);
+
+            deleteThumbnailFile(quiz.getThumbnailUrl());
+            quiz.changeThumbnailUrl(newThumbnailPath);
+        }
+    }
+
+    private void deleteThumbnailFile(String oldFilename) {
+        if (oldFilename.contains(DEFAULT)) {
+            return;
+        }
+
+        // oldFilename : /images/thumbnail/123asd.jpg
+        // filename : 123asd.jpg
+        String filename = oldFilename.substring(oldFilename.lastIndexOf("/") + 1);
+        Path filePath = Paths.get(uploadPath, filename).toAbsolutePath();
+
+        try {
+            boolean deleted = deleteIfExists(filePath);
+            if (deleted) {
+                log.info("기존 썸네일 삭제 완료 : {}", filePath);
+            } else {
+                log.info("기존 썸네일 존재 X : {}", filePath);
+            }
+        } catch (IOException e) {
+            log.error("기존 썸네일 삭제 중 오류 : {}", filePath);
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public QuizListPageResponse getQuizzes(String title, String creator, Pageable pageable) {
+
+        Page<Quiz> quizzes;
+
+        // 검색어가 있을 때
+        if (StringUtils.isBlank(title)) {
+            quizzes = quizRepository.findQuizzesByTitleContaining(title, pageable);
+        } else if (StringUtils.isBlank(creator)) {
+            quizzes = quizRepository.findQuizzesByCreator_NicknameContaining(creator, pageable);
+        } else { // 검색어가 없을 때 혹은 빈 문자열일 때
+            quizzes = quizRepository.findAll(pageable);
+        }
+
+        Page<QuizListResponse> quizListResponses = pageQuizToPageQuizListResponse(quizzes);
+
+        return toQuizListPageResponse(quizListResponses);
+    }
+
+    @Transactional(readOnly = true)
     public Quiz getQuizById(Long quizId) {
         return quizRepository
                 .findById(quizId)
