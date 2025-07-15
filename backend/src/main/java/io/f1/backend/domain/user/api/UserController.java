5ed35cc1c3500c739e7a03ddaff355abfc1b1d91
jiwon1217 <koagjw@gmail.com>
2025-07-15 16:37:30 +0900

    :bug: fix: @Controller -> @RestController 수정

--- backend/src/main/java/io/f1/backend/domain/user/api/UserController.java
+++ backend/src/main/java/io/f1/backend/domain/user/api/UserController.java
@@ -12,13 +12,13 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/user/me")
 public class UserController {
