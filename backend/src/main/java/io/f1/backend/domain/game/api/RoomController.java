2f85c233e7215a32fc4136d0384d9b5b67e49694
sehee <sskfro6@gmail.com>
2025-07-15 10:22:39 +0900

    :recycle: 미사용 import 제거

--- backend/src/main/java/io/f1/backend/domain/game/api/RoomController.java
+++ backend/src/main/java/io/f1/backend/domain/game/api/RoomController.java
@@ -5,11 +5,8 @@ import io.f1.backend.domain.game.dto.request.RoomCreateRequest;
 import io.f1.backend.domain.game.dto.request.RoomValidationRequest;
 import io.f1.backend.domain.game.dto.response.RoomCreateResponse;
 import io.f1.backend.domain.game.dto.response.RoomListResponse;
-
 import jakarta.validation.Valid;
-
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,9 +14,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/rooms")
