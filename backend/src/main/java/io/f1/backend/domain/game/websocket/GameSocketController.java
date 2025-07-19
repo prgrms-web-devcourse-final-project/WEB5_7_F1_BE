f25c170b561cb42d9eb6e933046e3bbf147b95ef
sehee <sskfro6@gmail.com>
2025-07-19 15:02:39 +0900

    :recycle: 충돌 해결

--- backend/src/main/java/io/f1/backend/domain/game/websocket/GameSocketController.java
+++ backend/src/main/java/io/f1/backend/domain/game/websocket/GameSocketController.java
@@ -1,11 +1,12 @@
 package io.f1.backend.domain.game.websocket;
 
+import static io.f1.backend.domain.game.websocket.WebSocketUtils.getSessionId;
+import static io.f1.backend.domain.game.websocket.WebSocketUtils.getSessionUser;
+
 import io.f1.backend.domain.game.app.GameService;
 import io.f1.backend.domain.game.app.RoomService;
 import io.f1.backend.domain.game.dto.ChatMessage;
-import io.f1.backend.domain.game.dto.MessageType;
 import io.f1.backend.domain.game.dto.request.DefaultWebSocketRequest;
-import io.f1.backend.domain.game.dto.response.GameStartResponse;
 import io.f1.backend.domain.user.dto.UserPrincipal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.Message;
@@ -42,20 +43,12 @@ public class GameSocketController {
     }
 
     @MessageMapping("/room/start/{roomId}")
-    public void gameStart(
-        @DestinationVariable Long roomId,
-        Message<DefaultWebSocketRequest<GameStartRequest>> message) {
     public void gameStart(@DestinationVariable Long roomId, Message<?> message) {
 
-        GameStartResponse gameStartResponse =
-            gameService.gameStart(roomId, message.getPayload().getMessage());
         UserPrincipal principal = getSessionUser(message);
 
-        GameStartResponse gameStartResponse = gameService.gameStart(roomId, principal);
+        gameService.gameStart(roomId,principal);
 
-        String destination = getDestination(roomId);
-
-        messageSender.send(destination, MessageType.GAME_START, gameStartResponse);
     }
 
     @MessageMapping("room/chat/{roomId}")
@@ -70,17 +63,6 @@ public class GameSocketController {
     public void playerReady(@DestinationVariable Long roomId, Message<?> message) {
 
         roomService.handlePlayerReady(roomId, getSessionId(message));
-    }
-
-    private static String getSessionId(Message<?> message) {
-        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
-        return accessor.getSessionId();
-    }
-
-    private static UserPrincipal getSessionUser(Message<?> message) {
-        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
-        Authentication auth = (Authentication) accessor.getUser();
-        return (UserPrincipal) auth.getPrincipal();
     }
 
     //todo 삭제
