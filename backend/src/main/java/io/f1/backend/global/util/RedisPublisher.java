1cea9a4ab35421e89bc036c712979b5dbf1a0f67
dlsrks1021 <dlsrks1021@gmail.com>
2025-07-24 17:18:56 +0900

    :recycle: refactor: 랭킹 조회 Redis 적용 (#111)

---
+++ backend/src/main/java/io/f1/backend/global/util/RedisPublisher.java
@@ -0,0 +1,20 @@
+package io.f1.backend.global.util;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RedisPublisher {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+    public static final String USER_NEW = "user-new";
+    public static final String USER_UPDATE = "user-update";
+    public static final String USER_DELETE = "user-delete";
+
+    public void publish(String channel, Object message) {
+        redisTemplate.convertAndSend(channel, message);
+    }
+}
