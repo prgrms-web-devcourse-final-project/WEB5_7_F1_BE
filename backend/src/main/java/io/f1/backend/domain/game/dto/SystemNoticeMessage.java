6ec049f0c7fe243b96fa7cadc27778c16877eb50
sehee <sskfro6@gmail.com>
2025-07-25 15:39:55 +0900

    :adhesive_bandage: systemNotice 메세지 수정

--- backend/src/main/java/io/f1/backend/domain/game/dto/SystemNoticeMessage.java
+++ backend/src/main/java/io/f1/backend/domain/game/dto/SystemNoticeMessage.java
@@ -5,8 +5,8 @@ public enum SystemNoticeMessage {
     EXIT(" 님이 퇴장하셨습니다"),
     CORRECT_ANSWER(" 님 정답입니다 !"),
     TIMEOUT("땡 ~ ⏰ 제한 시간 초과!"),
-    RECONNECT("⚠️ 님이 재연결 되었습니다. "),
-    RECONNECT_PRIVATE_NOTICE("⚠️ 다음 라운드부터 참여하실 수 있습니다");
+    RECONNECT(" 님이 재연결 되었습니다. "),
+    RECONNECT_PRIVATE_NOTICE("⚠️다음 라운드부터 참여하실 수 있습니다");
 
     private final String message;
 
