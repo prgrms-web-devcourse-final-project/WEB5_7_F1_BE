aea4c0141efacccd74804126f50136462001e513
hwangsehee <65377385+sehee123@users.noreply.github.com>
2025-07-27 17:33:01 +0900

    :adhesive_bandage: refactor : currentUserCount 응답 위치 변경
    * :adhesive_bandage: currentUserCount 응답 위치 변경
    
    * chore: Java 스타일 수정
    
    * :bug: RoomServiceTests 변경 적용
    
    * chore: Java 스타일 수정
    
    ---------
    
    Co-authored-by: github-actions <>

--- backend/src/main/java/io/f1/backend/domain/game/dto/response/RoomSettingResponse.java
+++ backend/src/main/java/io/f1/backend/domain/game/dto/response/RoomSettingResponse.java
@@ -1,4 +1,3 @@
 package io.f1.backend.domain.game.dto.response;
 
-public record RoomSettingResponse(
-        String roomName, int maxUserCount, int currentUserCount, boolean locked) {}
+public record RoomSettingResponse(String roomName, int maxUserCount, boolean locked) {}
