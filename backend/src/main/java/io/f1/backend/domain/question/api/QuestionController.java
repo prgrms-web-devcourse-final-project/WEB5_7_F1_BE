272a33df08eb1ab208c657f15cf22a1382c3e1a0
Lee Eunjoo (Silver) <80297399+silver-eunjoo@users.noreply.github.com>
2025-07-28 20:24:48 +0900

    :recycle: refactor : 퀴즈 수정, 문제 수정 API 리팩토링 (#146)
    * :recycle: refactor : 퀴즈 수정, 문제 수정 API 리팩토링
    
    * chore: Java 스타일 수정
    
    * :wrench: chore: 타이머 로그 기록
    
    * chore: Java 스타일 수정
    
    * :recycle: refactor : DTO Valid 추가
    
    * chore: Java 스타일 수정
    
    * :recycle: refactor: UPDATE시 문제 생성도 가능
    
    * chore: Java 스타일 수정
    
    * :wrench: chore: 타이머 관련 로그 수정
    
    * :recycle: refactor: 새로 생성 후 return
    
    ---------
    
    Co-authored-by: github-actions <>

--- backend/src/main/java/io/f1/backend/domain/question/api/QuestionController.java
+++ backend/src/main/java/io/f1/backend/domain/question/api/QuestionController.java
@@ -1,15 +1,12 @@
 package io.f1.backend.domain.question.api;
 
 import io.f1.backend.domain.question.app.QuestionService;
-import io.f1.backend.domain.question.dto.QuestionUpdateRequest;
 
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,21 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class QuestionController {
 
     private final QuestionService questionService;
-
-    @PutMapping("/{questionId}")
-    public ResponseEntity<Void> updateQuestion(
-            @PathVariable Long questionId, @RequestBody QuestionUpdateRequest request) {
-
-        if (request.content() != null) {
-            questionService.updateQuestionContent(questionId, request.content());
-        }
-
-        if (request.content() != null) {
-            questionService.updateQuestionAnswer(questionId, request.answer());
-        }
-
-        return ResponseEntity.noContent().build();
-    }
 
     @DeleteMapping("/{questionId}")
     public ResponseEntity<Void> deleteQuestion(@PathVariable Long questionId) {
