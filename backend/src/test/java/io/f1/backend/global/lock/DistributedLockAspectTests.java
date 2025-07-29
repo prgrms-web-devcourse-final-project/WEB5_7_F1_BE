f79c2727bb493ed0e4df964f438c53a826955b4f
limkanghyun <limkh06910691@gmail.com>
2025-07-29 09:25:05 +0900

    :recycle: refactor: 테스트 Order 제거

--- backend/src/test/java/io/f1/backend/global/lock/DistributedLockAspectTests.java
+++ backend/src/test/java/io/f1/backend/global/lock/DistributedLockAspectTests.java
@@ -18,7 +18,6 @@ import org.aspectj.lang.reflect.MethodSignature;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -69,7 +68,6 @@ class DistributedLockAspectTests {
         when(joinPoint.getArgs()).thenReturn(new Object[] {TEST_ROOM_ID});
     }
 
-    @Order(1)
     @DisplayName("락 획득 성공 시 정상적으로 메서드가 실행되고, 락 해제가 호출되는지 확인")
     @Test
     void testLock_Success() throws Throwable {
@@ -103,7 +101,6 @@ class DistributedLockAspectTests {
         }
     }
 
-    @Order(2)
     @DisplayName("락 획득 실패 시 CustomException(LOCK_ACQUISITION_FAILED)이 발생하는지 확인")
     @Test
     void testLock_FailToAcquireLock() throws Throwable {
@@ -138,7 +135,6 @@ class DistributedLockAspectTests {
         }
     }
 
-    @Order(3)
     @DisplayName("락 대기 중 인터럽트 발생 시 InterruptedException이 전파되는지 확인")
     @Test
     void testLock_InterruptedException() throws Throwable {
@@ -173,7 +169,6 @@ class DistributedLockAspectTests {
         }
     }
 
-    @Order(4)
     @DisplayName("락을 획득하지 않은 스레드가 unlock하지 않는지 확인")
     @Test
     void testLock_NotHeldByCurrentThread() throws Throwable {
@@ -208,7 +203,6 @@ class DistributedLockAspectTests {
         }
     }
 
-    @Order(5)
     @DisplayName("메서드 실행 중 예외 발생 시에도 락이 정상적으로 해제되는지 확인")
     @Test
     void testLock_ExceptionDuringExecution() throws Throwable {
@@ -246,7 +240,6 @@ class DistributedLockAspectTests {
         }
     }
 
-    @Order(6)
     @DisplayName("SpEL 파싱을 통한 동적 키 생성이 정상적으로 작동하는지 확인")
     @Test
     void testGetLockKey_WithSpELExpression() throws Throwable {
