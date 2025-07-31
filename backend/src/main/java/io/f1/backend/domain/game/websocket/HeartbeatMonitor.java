70fc1f8e442a8a4f75e449badcc031131a6303fc
hwangsehee <65377385+sehee123@users.noreply.github.com>
2025-07-31 19:22:52 +0900

    :sparkles: feat:하트비트 구현
    * :sparkles: 하트비트 구현
    
    * chore: Java 스타일 수정
    
    * :adhesive_bandage: pr리뷰 반영
    
    * chore: Java 스타일 수정
    
    * :bulb: FE 개발될때까지 주석처리
    
    * chore: Java 스타일 수정
    
    * :adhesive_bandage: PR리뷰 반영
    
    ---------
    
    Co-authored-by: github-actions <>

---
+++ backend/src/main/java/io/f1/backend/domain/game/websocket/HeartbeatMonitor.java
@@ -0,0 +1,89 @@
+package io.f1.backend.domain.game.websocket;
+
+import static io.f1.backend.domain.game.websocket.WebSocketUtils.getUserDestination;
+
+import io.f1.backend.domain.game.app.RoomService;
+import io.f1.backend.domain.game.dto.MessageType;
+import io.f1.backend.domain.game.dto.response.HeartbeatResponse;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.messaging.simp.user.SimpSession;
+import org.springframework.messaging.simp.user.SimpUser;
+import org.springframework.messaging.simp.user.SimpUserRegistry;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class HeartbeatMonitor {
+
+    private static final String DIRECTION = "serverToClient";
+    private static final int MAX_MISSED_HEARTBEATS = 3;
+    private static final long HEARTBEAT_CHECK_INTERVAL_MS = 15000L;
+
+    private final Map<String, Integer> missedPongCounter = new ConcurrentHashMap<>();
+
+    private final MessageSender messageSender;
+    private final RoomService roomService;
+    private final SimpUserRegistry simpUserRegistry;
+
+    @Scheduled(fixedDelay = HEARTBEAT_CHECK_INTERVAL_MS)
+    public void monitorClientHeartbeat() {
+        /* user 없으면 skip */
+        if (simpUserRegistry.getUserCount() == 0) {
+            return;
+        }
+
+        simpUserRegistry
+                .getUsers()
+                .forEach(
+                        user ->
+                                user.getSessions()
+                                        .forEach(session -> handleSessionHeartbeat(user, session)));
+    }
+
+    private void handleSessionHeartbeat(SimpUser user, SimpSession session) {
+        String sessionId = session.getId();
+
+        /* ping */
+        messageSender.sendPersonal(
+                getUserDestination(),
+                MessageType.HEARTBEAT,
+                new HeartbeatResponse(DIRECTION),
+                user.getName());
+
+        // todo FE 개발 될때까지 주석 처리
+        //        missedPongCounter.merge(sessionId, 1, Integer::sum);
+        //        int missedCnt = missedPongCounter.get(sessionId);
+        //
+        //        /* max_missed_heartbeats 이상 pong 이 안왔을때 - disconnect 처리 */
+        //        if (missedCnt >= MAX_MISSED_HEARTBEATS) {
+        //
+        //            Principal principal = user.getPrincipal();
+        //
+        //            if (principal instanceof UsernamePasswordAuthenticationToken token
+        //                    && token.getPrincipal() instanceof UserPrincipal userPrincipal) {
+        //
+        //                Long userId = userPrincipal.getUserId();
+        //                Long roomId = roomService.getRoomIdByUserId(userId);
+        //
+        //                roomService.disconnectOrExitRoom(roomId, userPrincipal);
+        //            }
+        //            cleanSession(sessionId);
+        //        }
+    }
+
+    public void resetMissedPongCount(String sessionId) {
+        missedPongCounter.put(sessionId, 0);
+    }
+
+    public void cleanSession(String sessionId) {
+        missedPongCounter.remove(sessionId);
+    }
+}
